b67a2577faf749f8d67d598d28cdbe6054fae33b
MitchellJBrown31 <Mitchell.j.brown.31@gmail.com>
2023-07-15 19:02:39 -0400

    prefab edits and game rules

---
+++ Space Sucks/Assets/Murder.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Murder : MonoBehaviour
+{
+    public float duration = 1000.0f;
+
+    private void Update()
+    {
+        duration -= Time.deltaTime;
+
+        if(duration<0)
+        {
+            if (GameObject.FindGameObjectWithTag("Player").transform.position.y - transform.position.y > 1) Lose();
+        }
+        if(duration<-3)
+            GameObject.Destroy(gameObject);
+
+    }
+
+    private void Lose()
+    {
+        Debug.Log("You Lose");
+    }
+}
--- Space Sucks/Assets/Stairwell.cs
+++ Space Sucks/Assets/Stairwell.cs
@@ -2,16 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gate : MonoBehaviour
+public class Stairwell : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        //if (other.tag != "Player") return;
+        if (other.tag != "Player") return;
 
         RoomManager.Instance.roomOffset += new Vector3(0, -20, 0);
         RoomManager.Instance.flipped = !RoomManager.Instance.flipped;
         RoomManager.Instance.LoadNextRoom();
     }
 }
-
-
